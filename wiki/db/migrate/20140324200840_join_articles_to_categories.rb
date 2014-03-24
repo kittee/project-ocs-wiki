9843650c3418f424d3a533cc27bc72a9c0083bf5
matthova <matthova87@gmail.com>
2014-03-24 15:30:09 -0500

    fixed migrate

--- wiki/db/migrate/20140324200840_join_articles_to_categories.rb
+++ wiki/db/migrate/20140324200840_join_articles_to_categories.rb
@@ -1,6 +1,6 @@
 class JoinArticlesToCategories < ActiveRecord::Migration
   def up
-    create_table :articles_categories, id => false do |t|
+    create_table :articles_categories, :id => false do |t|
       t.integer :article_id
       t.integer :category_id
     end
