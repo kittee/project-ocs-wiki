592b48aa21ceda5db5eee1c67171fb02aff77b01
Cara Heacock <caraheacock@gmail.com>
2014-03-26 15:33:58 -0500

    Admin accounts added. Remember to change your password ASAP when launched to Heroku.

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,34 @@ Category.create([
   { name: "coding_language" },
   { name: "miscellaneous" }
 ])
+
+cara = User.create(
+  email: "caraheacock@gmail.com",
+  username: "cara",
+  fname: "Cara",
+  lname: "Heacock",
+  password: "cara",
+  password_confirmation: "cara"
+)
+
+matt = User.create(
+  email: "me@matthova.com",
+  username: "matt",
+  fname: "Matt",
+  lname: "Hova",
+  password: "matt",
+  password_confirmation: "matt"
+)
+
+andy = User.create(
+  email: "avondohren@gmail.com",
+  username: "andy",
+  fname: "Andy",
+  lname: "von Dohren",
+  password: "andy",
+  password_confirmation: "andy"
+)
+
+cara.update_attribute(:admin, true)
+matt.update_attribute(:admin, true)
+andy.update_attribute(:admin, true)
